cb7b6b4d50d5f4b0cde06196c5fd3975cdf4c78b
George Wilkins <georgewilkins99@gmail.com>
2016-09-11 15:24:10 +0100

    Removed unused private fields

--- TextBasedAdventure/Classes/Player.cs
+++ TextBasedAdventure/Classes/Player.cs
@@ -3,49 +3,27 @@ namespace TextBasedAdventure
 {
     class Player
     {
-        private int OneHandedBonus = 0;
-        private int TwoHandedBonus = 0;
-        private int RangedBonus = 0;
-        private int MagicalBonus = 0;
-        private int PhysicalAttack = 0;
-        private int PhysicalDefence = 0;
-        private int MagicalAttack = 0;
-        private int MagicalDefence = 0;
-        private string Name = null;
-        private string Gender = null;
-        private string Race = null;
-        private string OneHandedWeapon = null;
-        private string TwoHandedWeapon = null;
-        private string Shield = null;
-        private string RangedWeapon = null;
-        private string MagicalWeapon = null;
-        private string DefaultWeapon = null;
-        private string DefaultShield = null;
-        private string ClassName = null;
-        private string PrimaryWeapon = null;
-        private string PrimaryShield = null;
-
-        public int oneHandedBonus { get; set; }
-        public int twoHandedBonus { get; set; }
-        public int rangedBonus { get; set; }
-        public int magicalBonus { get; set; }
-        public int physicalAttack { get; set; }
-        public int physicalDefence { get; set; }
-        public int magicalAttack { get; set; }
-        public int magicalDefence { get; set; }
-        public string name { get; set; } 
-        public string gender { get; set; }
-        public string race { get; set; }
-        public string oneHandedWeapon { get; set; }
-        public string twoHandedWeapon { get; set; }
-        public string shield { get; set; }
-        public string rangedWeapon { get; set; }
-        public string magicalWeapon { get; set; }
-        public string defaultWeapon { get; set; }
-        public string defaultShield { get; set; }
-        public string className { get; set; }
-        public string primaryWeapon { get; set; }
-        public string primaryShield { get; set; }
+        public int oneHandedBonus { get; set; } = 0;
+        public int twoHandedBonus { get; set; } = 0;
+        public int rangedBonus { get; set; } = 0;
+        public int magicalBonus { get; set; } = 0;
+        public int physicalAttack { get; set; } = 0;
+        public int physicalDefence { get; set; } = 0;
+        public int magicalAttack { get; set; } = 0;
+        public int magicalDefence { get; set; } = 0;
+        public string name { get; set; } = null;
+        public string gender { get; set; } = null;
+        public string race { get; set; } = null;
+        public string oneHandedWeapon { get; set; } = null;
+        public string twoHandedWeapon { get; set; } = null;
+        public string shield { get; set; } = null;
+        public string rangedWeapon { get; set; } = null;
+        public string magicalWeapon { get; set; } = null;
+        public string defaultWeapon { get; set; } = null;
+        public string defaultShield { get; set; } = null;
+        public string className { get; set; } = null;
+        public string primaryWeapon { get; set; } = null;
+        public string primaryShield { get; set; } = null;
 
         public Player()
         {
--- TextBasedAdventure/Classes/Weapon.cs
+++ TextBasedAdventure/Classes/Weapon.cs
@@ -2,11 +2,6 @@
 {
     class Weapon : Item
     {
-        private string PhysicalAttack;
-        private string MagicalAttack;
-        private string PhysicalDefence;
-        private string MagicalDefence;
-
         public string physicalAttack { get; set; }
         public string magicalAttack { get; set; }
         public string physicalDefence { get; set; }
